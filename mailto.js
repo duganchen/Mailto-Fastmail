427ad48e375c29838b3d42f0a4939edf03f1853e
Dugan Chen <thedoogster@gmail.com>
2011-01-30 14:53:27 -0800

    Initial commit. Tested with Craigslist's mailto urls.

---
+++ mailto.js
@@ -0,0 +1,16 @@
+$(function() {
+	$.each($("a"), function(indexInArray, valueOfElement) {
+		var url = "http://www.fastmail.fm/action/compose/"
+		var href = null;
+		var parameters = null;
+		href = $(this).attr("href").split(':', 2);
+		if (href.length === 2 && href[0] === 'mailto') {
+			parameters = href[1].split('?');
+			url = url + '?to=' + parameters[0];
+			if (parameters.length > 1) {
+				url = url + '&' + parameters[1];
+			}
+			$(this).attr("href", url);
+		}
+	});
+});
